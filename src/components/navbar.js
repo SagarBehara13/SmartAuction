9b45a1c3c698c61a070ec33fb5fc8263a7365e66
Mithil Jain <mithiljain8@gmail.com>
2020-11-21 22:07:14 +0530

    navbar design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import MetaMaskLoginButton from "react-metamask-login-button";
+import logo from '../logo.png';
 
 class NavBar extends Component {
     constructor(props) {
@@ -22,26 +23,43 @@ class NavBar extends Component {
     render() {
         return (
             <React.Fragment>
-                <Navbar color="black top-navbar" dark fixed expand="md">
-                    <div className="container">
-                    <NavbarToggler dark onClick={this.toggleNav} />
-                    <NavbarBrand className="mr-auto nav-brand" href="/home">
-                        <span className="branding">DeDonate</span>
-                    </NavbarBrand>
-                    <Collapse isOpen={this.state.isNavOpen} navbar className="">
-                        <div className="flex-grow-1"></div>
-                        <Nav className="align-items-end" navbar>
-                            <NavItem>
-                                <NavLink onClick={this.toggleNav} className="nav-link" to="/explore">
-                                    {/*<i className="fa fa-child fa-lg"></i> */}
-                                    <div className="connect-btn"><MetaMaskLoginButton /></div>
-                                    {/*<span className="main-btn">Explore</span>*/}
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
+                <div className='navbar-custom'>
+                    <div className='container'>
+                        <div className='navbar-contain'>
+                            <div className='brand-menu'>
+                                <div className='logo'><img src={logo} className="logo-app" alt="logo" /></div>
+                                <div className='app-name'>Smart Auction</div>
+                            </div>
+                            <div className='main-menu'>
+                                <div>Home</div>
+                                <div>listing</div>
+                                <div>Somename</div>
+                            </div>
+                        </div>
                     </div>
-                </Navbar>
+
+                </div>
+
+                {/*<Navbar color="black top-navbar" dark fixed expand="md">*/}
+                {/*    <div className="container">*/}
+                {/*    <NavbarToggler dark onClick={this.toggleNav} />*/}
+                {/*    <NavbarBrand className="mr-auto" href="/home">*/}
+                {/*        <span className="branding">DeDonate</span>*/}
+                {/*    </NavbarBrand>*/}
+                {/*    /!*<Collapse isOpen={this.state.isNavOpen} navbar className="">*!/*/}
+                {/*    /!*    <div className="flex-grow-1"></div>*!/*/}
+                {/*    /!*    <Nav className="align-items-end" navbar>*!/*/}
+                {/*    /!*        <NavItem>*!/*/}
+                {/*    /!*            <NavLink onClick={this.toggleNav} className="nav-link" to="/explore">*!/*/}
+                {/*    /!*                /!*<i className="fa fa-child fa-lg"></i> *!/*!/*/}
+                {/*    /!*                <div className="connect-btn"><MetaMaskLoginButton /></div>*!/*/}
+                {/*    /!*                /!*<span className="main-btn">Explore</span>*!/*!/*/}
+                {/*    /!*            </NavLink>*!/*/}
+                {/*    /!*        </NavItem>*!/*/}
+                {/*    /!*    </Nav>*!/*/}
+                {/*    /!*</Collapse>*!/*/}
+                {/*    </div>*/}
+                {/*</Navbar>*/}
             </React.Fragment>
         )
     }
