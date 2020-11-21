56c84475d6c0f788b8dcea57f61b5e50ee5b0d13
Mithil Jain <mithiljain8@gmail.com>
2020-11-21 22:44:04 +0530

    home page branding UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import banner from '../images/banner.png'
 
 function Home (){
     return(
         <div>
-            <p>Hello</p>
+            <div>
+                <img src={banner} className='banner'/>
+            </div>
+            <div className='welcome'>
+                <div className='welcome-container'>
+                    <p className='welcome-msg'>Welcome to Smart Auction</p>
+                    <button className='start-bidding'>Start Bidding</button>
+                </div>
+
+            </div>
+            <div className='container'>
+
+            </div>
         </div>
+
     )
 }
 
