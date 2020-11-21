80fbe9a1b5e68e3927cfbf9b5022e1f56c384575
Yash Agrawal <yashagrawal18@outlook.com>
2020-11-21 20:35:31 +0530

    Add gitignore and minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href=""
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -21,7 +21,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href=""
+                  href="/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
